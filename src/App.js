c7305448407729759252f8f9d5c06dce473b1ae0
Ruddy marc <pc-de-ruddy@LAPTOP-OONQKG13.gp>
2023-03-13 07:27:14 -0400

    feat: add an guard to limit acces on protected ressources

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './Screens/Layout';
 import ProtectedLayout from './Screens/ProtectedLayout';
 import Home from './Screens/Home';
@@ -13,13 +13,21 @@ function App() {
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
           <Route path='login' element={<Login />} />
-          <Route path='protected' element={<ProtectedLayout />}>
+          <Route path='protected' element={<Guard />}>
             <Route index element={<Protected />} />
           </Route>
         </Route>
       </Routes>
     </BrowserRouter>
   );
+}
+
+function Guard() {
+  return (
+    window.localStorage.getItem('token')
+      ? <ProtectedLayout />
+      : <Navigate to='/login' />
+  )
 }
 
 export default App;
